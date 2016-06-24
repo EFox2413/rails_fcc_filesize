db41d1d15e4f7a07d65998e55221594487257e8a
gitolite <EFox2413@gmail.com>
2016-06-24 07:23:58 -0700

    removed dev environment variable for certs

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-# required for https requests in ruby to work right
-ENV["SSL_CERT_FILE"] = "C:\\Ruby22\\cacert.pem"
